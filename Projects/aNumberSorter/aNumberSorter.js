73235853b92fb1ef5b0ffdceed9cb8ae1f62af49
WORK1402 <166902905+WORK1402@users.noreply.github.com>
2024-08-19 15:37:48 +0530

    Update aNumberSorter.js

--- Projects/aNumberSorter/aNumberSorter.js
+++ Projects/aNumberSorter/aNumberSorter.js
@@ -68,3 +68,7 @@ const insertionSort = (array) => {
 }
 
 sortButton.addEventListener("click", sortInputArray);
+ document.getElementById('backButton').addEventListener('click', function() {
+    // If you want to go back in the history stack
+    window.history.back();
+ });
